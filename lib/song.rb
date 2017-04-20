3497e9f75cca7641fc76879c6630f4ebc7fb7af3
Dan Strong <dangaston@gmail.com>
2017-04-20 19:30:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,51 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
 
   def save
-    self.class.all << self
+     self.class.all << self
+  end
+   
+  def self.create
+    song = self.new
+    @@all << song
+    song
   end
 
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all << song
+    song
+  end
+
+  def self.create_by_name(song_name) # not yet working
+    song = self.new
+    song.name = song_name
+    @@all << song
+    song
+  end
+
+    def find_by_name(name)
+      self.name
+    end
+
+    def create_from_filename(file_name)
+      song = self.new
+      @artist_name = file_name.split(' - ').first
+      @song = file_name.split(' - ').last.split('.').first
+      
+      @@all << song
+      song
+    end
+    
+    def self.destroy_all
+      self.all.clear
+    end
+    
 end
+ 
